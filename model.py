db97b84c26e35fe121a9bde6787c3a0566b81ffd
Salah <47069801+OutWrest@users.noreply.github.com>
2021-02-21 15:39:16 -0600

    Fixed file read error

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
-with open('data/test.csv') as f:
-    test_data = [ int(l.split(',')) for l in f.read().splitlines()[1:] ]
+with open('data/test.csv', 'r') as f:
+    test_data = [ [ int(px) for px in l.split(',') ] for l in f.read().splitlines()[1:] ]
 
-with open('data/test.csv') as f:
-    train_data = [ int(l.split(',')) for l in f.read().splitlines()[1:] ]
+with open('data/test.csv', 'r') as f:
+    train_data = [ [ int(px) for px in l.split(',') ] for l in f.read().splitlines()[1:] ]
 
 # TODO
