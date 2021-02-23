df6fd51979f161fdc384ba4a87523bc1a3a2288a
Salah <47069801+OutWrest@users.noreply.github.com>
2021-02-22 22:11:35 -0600

    forawrd propergate sample

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from model import Model
 from layer import Layer
 
-# model = Model()
-# model.addLayer(10, input=(784, 1))
-# model.addLayer(10)
+model = Model()
+model.addLayer(10, input_shape=2)
+model.addLayer(10)
+
+print(model.layers[0].neurons)
+
+print(model.predict([1.0, 1.0]))
