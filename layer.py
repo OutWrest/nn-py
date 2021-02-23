38e229ca0f90efce2a58c37e5c22695e73259854
Salah <47069801+OutWrest@users.noreply.github.com>
2021-02-22 22:11:35 -0600

    forawrd propergate sample

--- layer.py
+++ layer.py
@@ -2,8 +2,10 @@ from typing import Callable, List
 from node import Node
 
 class Layer:
-    def __init__(self, num_weights: int) -> None:
-        self.nodes = [ Node(num_weights) for _ in range(num_weights) ]
+    def __init__(self, num_weights: int, neurons: int) -> None:
+        self.neurons = neurons
+        self.num_weights = num_weights
+        self.nodes = [ Node(num_weights) for _ in range(neurons) ]
     
     def get_activations(self, prev_activations: List[float], activation_function: Callable) -> List[float]:
         return [ n.get_activation(prev_activations, activation_function) for n in self.nodes ]
--- model.py
+++ model.py
@@ -1,6 +1,36 @@
+import math
+from typing import List
+from layer import Layer
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 class Model:
-    def __init__(self,) -> None:
-        pass
+    def __init__(self) -> None:
+        self.layers = []
+
+    def addLayer(self, neurons: int, input_shape: int = None) -> None:
+        if not self.layers:
+            if not input_shape:
+                raise Exception('Input shape not defined on first layer')
+            self.layers.append( Layer(input_shape, neurons ))
+        else:
+            # Sets the input shape neurons from last layer
+            self.layers.append( Layer(self.layers[-1].neurons, neurons ))
+    
+    def predict(self, inputs: List[float]) -> List[float]:
+        # There are layers that exist
+        assert(self.layers) 
+        # The first layer accepts input shape
+        assert(self.layers[0].num_weights == len(inputs))
+
+        for layer in self.layers:
+            inputs = layer.get_activations(inputs, sigmoid)
+
+        return inputs
 
 if __name__ == "__main__":
     pass
