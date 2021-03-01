5fe0be6d9c823ee6da972684ca3cee1101291ac8
Salah <47069801+OutWrest@users.noreply.github.com>
2021-02-28 18:46:10 -0600

    backpropagate only last layer

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ from model import Model
 from layer import Layer
 
 model = Model()
-model.addLayer(10, input_shape=2)
-model.addLayer(10)
+model.addLayer(2, input_shape=2, )
+model.addLayer(2)
 
-print(model.layers[0].neurons)
-print(model.predict([1.0, 1.0]))
+weights = [
+    [
+        [
+            .15, .20
+        ],
+        [
+            .25, .30
+        ]
+    ],
+    [
+        [
+            .40, .45
+        ],
+        [
+            .50, .55
+        ]
+    ]    
+]
+
+biases = [
+    [
+        .35, .35
+    ],
+    [
+        .60, .60
+    ]
+]
+
+expected = [
+    .01, .99
+]
+
+model.loadModel( weights, biases )
+
+predicted = model.predict([.05, .10])
+#print(model.local_cost(predicted, expected))
+model.backpropagate([.05, .10], expected)
--- model.py
+++ model.py
@@ -22,25 +22,70 @@ class Model:
             # Sets the input shape neurons from last layer
             self.layers.append( Layer(self.layers[-1].neurons, neurons ))
     
-    def local_cost(self, output_activations: List[float], expected_activations: List[float]):
+    def local_cost(self, output_activations: List[float], expected_activations: List[float]) -> float:
         assert len(output_activations) == len(expected_activations)
         
         loss = 0
         for predicted, expected in zip(output_activations, expected_activations):
-            loss += pow(predicted-expected, 2)
+            loss += pow(predicted-expected, 2)/2
 
         return loss
     
+    def __cost(self, target: List[float], out: List[float]) -> List[float]:
+        return [ pow(t_n - o_n, 2)/2 for t_n, o_n in zip(target, out) ]
+    
+    def __d_cost(self, target: List[float], out: List[float]) -> List[float]:
+        return [ (t_n - o_n) for t_n, o_n in zip(target, out)]
+
+    def __predict(self, activations: List[float]) -> List[List[float]]:
+        ret = []
+
+        for layer in self.layers:
+            activations = layer.get_activations(activations, sigmoid)
+            ret.append(activations)
+        
+        return ret
+    
+    def loadModel(self, weights: List[List[List[float]]], biases: List[List[float]]) -> None:
+        for lweights, lbiases, layer in zip(weights, biases, self.layers):
+            layer.load(lweights, lbiases)
+            
     def predict(self, inputs: List[float]) -> List[float]:
         # There are layers that exist
         assert(self.layers) 
         # The first layer accepts input shape
         assert(self.layers[0].num_weights == len(inputs))
 
-        for layer in self.layers:
+        for i, layer in enumerate(self.layers):
             inputs = layer.get_activations(inputs, sigmoid)
+            print(f"[Layer {i}] {inputs}")
 
         return inputs
+    
+    def __backpropagate(self, activations, prev_activations, expdected: List[float], learning_rate: float) -> None:
+
+        # parial derivative with respect to cost
+        cost_t = self.__d_cost(activations[-1], expected)
+        print(cost_t)
+        
+        # parial derivative with respect to activation function
+        act_t = [ act*(1-act) for act in activations[-1] ]
+        print(act_t)
+
+        # partial derivative with respect to weights (everything)
+        wei_t = [ cos_n * act_n * w_n for cos_n, act_n, w_n in zip(cost_t, act_t, activations[-2])]
+        print(wei_t)
+
+        # individal weight changes?
+        n_wei = [[ weight - (learning_rate * weight_d) for weight in weights] for weights, weight_d in zip(weights_l, wei_t)]
+        print(n_wei)
+    
+    def backpropagate(self, a_input: List[float], expected: List[float], learning_rate: float = 0.5) -> None:
+        activations = self.__predict(a_input)
+        weights = [[node.weights for node in layer.nodes] for layer in self.layers]     
+        
+        #for acts, weis in zip(activations, weis):
+        #
 
 if __name__ == "__main__":
     pass
