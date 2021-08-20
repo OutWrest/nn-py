625ddd60478300ac598b6d5a64f6f4d88229235f
Salah <47069801+OutWrest@users.noreply.github.com>
2021-08-20 13:33:24 -0500

    Modified backprop

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import math
+from tkinter.constants import E
 from typing import List
 from layer import Layer
 from functools import reduce, total_ordering
@@ -46,9 +47,9 @@ class Model:
         
         return ret
     
-    def loadModel(self, weights: List[List[List[float]]], biases: List[List[float]]) -> None:
-        for lweights, lbiases, layer in zip(weights, biases, self.layers):
-            layer.load(lweights, lbiases)
+    def loadModel(self, weights: List[List[List[float]]], biases: List[float]) -> None:
+        for lweights, lbias, layer in zip(weights, biases, self.layers):
+            layer.load(lweights, lbias)
             
     def predict(self, inputs: List[float]) -> List[float]:
         # There are layers that exist
@@ -63,7 +64,6 @@ class Model:
         return inputs
     
     def __backpropagate(self, activations, prev_activations, expdected: List[float], learning_rate: float) -> None:
-
         # parial derivative with respect to cost
         cost_t = self.__d_cost(activations[-1], expected)
         print(cost_t)
@@ -80,12 +80,35 @@ class Model:
         n_wei = [[ weight - (learning_rate * weight_d) for weight in weights] for weights, weight_d in zip(weights_l, wei_t)]
         print(n_wei)
     
+    def __local_diff(self, activations: List[float], expected: List[float]) -> List[float]:
+        return [ (act_n - exp_n) for act_n, exp_n in zip(activations, expected) ]
+    
     def backpropagate(self, a_input: List[float], expected: List[float], learning_rate: float = 0.5) -> None:
         activations = self.__predict(a_input)
-        weights = [[node.weights for node in layer.nodes] for layer in self.layers]     
+        error = self.__cost(activations[-1], expected)
+        e_total = reduce(lambda x, y: x + y, error)
+
+        print(f"[Error] {error}]")
+        print(f"[Error Total] {e_total}")
+
+        # Backpropagate
+        for i in range(len(self.layers) - 1, -1, -1):
+            layer = self.layers[i]
+            prev_layer = self.layers[i-1]
+
+        ll_c = self.__local_diff(activations[1], expected)
+        l1_d = [ ll_w * (1 - ll_w) for ll_w in activations[1]]
+        l1_h = activations[~1]
+
+        for d, h, c, weights, i in zip(l1_d, l1_h, ll_c, self.layers[-1].get_weights(), range(len(l1_d))):
+            for w in weights:
+                change = w - (learning_rate * d * h * c)
+                print(f"[Layer {i}] {w} - ({d} * {h} * {c}) \t= {change}") 
+
+        print(f"[Local ll_c] {ll_c}")
+        print(f"[Local l1_d] {l1_d}")
+
         
-        #for acts, weis in zip(activations, weis):
-        #
 
 if __name__ == "__main__":
     pass
