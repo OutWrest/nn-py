3f58b5757acd8f9db39fcb70db292b7d6c7cbeb7
Salah <47069801+OutWrest@users.noreply.github.com>
2021-08-21 00:45:57 -0500

    backprop on rewritten model

--- rewritten/model.py
+++ rewritten/model.py
@@ -1,6 +1,6 @@
 import math
 
-from typing import List
+from typing import Callable, List
 from layer import Layer
 from activation import sigmoid, d_sigmoid
 
@@ -51,8 +51,30 @@ class Model:
             assert len(layer.weights) == len(self.layers[-1])
 
         self.layers.append(layer)
+    
+    def __fpropagate(self, input_data: List[float], activation: Callable = sigmoid) -> List[float]:
+        """
+        Performs forward propagation on the model.
+        :param input_data: The input data.
+        :param activation: The activation function.
+        :return: A list of activations.
+        """
+        ret = []
 
-    def forwardpropagate(self, input_data: list) -> list:
+        if len(self.layers) == 0:
+            raise ValueError("Model has no layers.")
+        
+        if len(input_data) != self.input_shape[0]:
+            raise ValueError("Input data has invalid shape.")
+
+        output_data = input_data
+        for layer in self.layers:
+            output_data = layer.forwardpropagate(output_data, activation)
+            ret.append(output_data)
+
+        return ret
+
+    def forwardpropagate(self, input_data: List[float], activation: Callable = sigmoid) -> list:
         """
         Propagates the input data through the model.
         :param input_data: The input data.
@@ -67,12 +89,32 @@ class Model:
 
         output_data = input_data
         for layer in self.layers:
-            output_data = layer.forwardpropagate(output_data, sigmoid)
+            output_data = layer.forwardpropagate(output_data, activation)
         return output_data
     
-    def fit(self, input_data: list, expected_data: list, learning_rate: float = 0.05, epochs: int = 100) -> None:
+    def get_error(self, input_data: List[float], expected: List[float], activation: Callable = sigmoid) -> float:
         """
-        Trains the model using the given data.
+        Calculates the error of the model of a specific input_data.
+        :param input_data: The input data.
+        :param expected: The expected output data.
+        :param activation: The activation function.
+        :return: The error.
+        """
+        if len(self.layers) == 0:
+            raise ValueError("Model has no layers.")
+        
+        if len(input_data) != self.input_shape[0]:
+            raise ValueError("Input data has invalid shape.")
+
+        if len(expected) != len(self.layers[-1]):
+            raise ValueError("Expected output data has invalid shape.")
+
+        output_data = self.forwardpropagate(input_data, activation)
+        return self.t_error(expected, output_data)
+
+    def train(self, input_data: list, expected_data: list, learning_rate: float = 0.5, epochs: int = 100, activation: Callable = sigmoid) -> None:
+        """
+        Trains the model using the given data. Assumes a batch size of 1 for now.
         :param input_data: The input data.
         :param expected_data: The expected data.
         :param learning_rate: The learning rate.
@@ -85,12 +127,49 @@ class Model:
         if len(input_data) != self.input_shape[0]:
             raise ValueError("Input data has invalid shape.")
 
-        if len(expected_data) != self.output_shape[0]:
+        if len(expected_data) != len(self.layers[-1]):
             raise ValueError("Expected data has invalid shape.")
 
+        weights_deltas = []
+
+        output = self.forwardpropagate(input_data, activation)
+        activations = self.__fpropagate(input_data, activation)
+
+        d_error_wrt_out = [-(target-out) for target, out in zip(expected_data, output)]
+        #print(d_error_wrt_out)
+        d_out_wrt_error = [out * (1 - out) for out in output]
+        #print(d_out_wrt_error)
+        pd_out_wrt_w = [activations[-2][i] for i in range(len(d_out_wrt_error))]
+        #print(pd_out_wrt_w)
+
+        deltas = []
+        for et, on, nw, weights in zip(d_error_wrt_out, d_out_wrt_error, pd_out_wrt_w, self.layers[-1].weights):
+            delta = []
+            for w in weights:
+                change = w - (learning_rate * on * nw * et)
+                delta.append(change)
+                print(f"[Delta] {str(w): <4} - ({on: <20} * {nw: <20} * {et: <20}) = {change}")
+            deltas.append(delta)
+        weights_deltas.append(deltas)
+
+        pd_e_wrt_out = [[e_o * o_n * self.layers[-1].weights[w][i] for e_o, o_n, w in zip(d_error_wrt_out, d_out_wrt_error, range(2))] for i in range(len(self.layers[-1].weights[0]))]
+
+        e_total_out = [sum(e_wrt_out) for e_wrt_out in pd_e_wrt_out]
+        d_pd_out_wrt_w = [act * (1 - act) for act in pd_out_wrt_w]
         
+        deltas = []
+        for et, inp, ow, weights in zip(e_total_out, input_data, d_pd_out_wrt_w, self.layers[0].weights):
+            delta = []
+            for w in weights:
+                change = w - (learning_rate * inp * ow * et)
+                delta.append(change)
+                print(f"[Delta] {str(w): <4} - ({inp: <20} * {ow: <20} * {et: <20}) = {change}")
+            deltas.append(delta)
+        weights_deltas.append(deltas)
 
-
+        # Change weights
+        for layer, deltas in zip(self.layers, weights_deltas):
+            layer.weights = [w - d for w, d in zip(layer.weights, deltas)]
 
 if __name__ == "__main__":
     # https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
@@ -112,7 +191,10 @@ if __name__ == "__main__":
     ]
     example.layers[1].bias = .60
 
-    print(example.forwardpropagate([.05, .10]))
+    # print(example.forwardpropagate([.05, .10]))
+    print(f"Total Error: {example.get_error([.05, .10], [.01, .99])}")
 
     # Backward propagation
+
+    example.train([.05, .10], [.01, .99])
 
